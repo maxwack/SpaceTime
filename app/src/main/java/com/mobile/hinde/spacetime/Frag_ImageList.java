5d83f990ecb7e377b75217e1e19a3bc18839fa82
max <wack.max@gmail.com>
2019-03-06 09:43:27 +0900

    Use of URL instead of image name

--- app/src/main/java/com/mobile/hinde/spacetime/Frag_ImageList.java
+++ app/src/main/java/com/mobile/hinde/spacetime/Frag_ImageList.java
@@ -1,12 +1,19 @@
 package com.mobile.hinde.spacetime;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +21,24 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.mobile.hinde.utils.Grid_Adapter;
 import com.mobile.hinde.utils.UserSettings;
 
+import org.json.JSONException;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Frag_ImageList extends Fragment {
 
@@ -42,25 +58,28 @@ public class Frag_ImageList extends Fragment {
         View view = inflater.inflate(R.layout.frag_image_list,
                 container, false);
 
-        dbInstance.collection("users").document(instance.getUserId()).collection("MOON").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+        dbInstance.collection("users").document(instance.getUserId()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
-                    List<String> list = new ArrayList<>();
-                    for (QueryDocumentSnapshot document : task.getResult()) {
-                        list.add(document.getId());
-                    }
+                    DocumentSnapshot document = task.getResult();
+                    Map<String,Object> userMap = document.getData();
 
                     // Create LinearLayout
-                    LinearLayout ll = new LinearLayout(getActivity());
+                    final LinearLayout ll = new LinearLayout(getActivity());
                     ll.setOrientation(LinearLayout.VERTICAL);
                     TextView title = createTextView();
                     View line = createLine();
+
+
+
+                    RecyclerView rv = createRecycler((ArrayList<String>) userMap.get("MOON"));
 
                     LinearLayout root = getView().findViewById(R.id.rootLayer);
 
                     ll.addView(title);
                     ll.addView(line);
+                    ll.addView(rv);
 
                     root.addView(ll);
 
@@ -100,5 +119,18 @@ public class Frag_ImageList extends Fragment {
         v .setLayoutParams(params);
         v.setBackgroundColor(ContextCompat.getColor(getActivity(),R.color.colorPrimary));
         return v;
+    }
+
+    private RecyclerView createRecycler(ArrayList<String> imgList){
+        final RecyclerView rv = new RecyclerView(getActivity());
+        RecyclerView.LayoutParams params = new RecyclerView.LayoutParams(RecyclerView.LayoutParams.MATCH_PARENT,RecyclerView.LayoutParams.MATCH_PARENT);
+        rv.setLayoutParams(params);
+
+        Grid_Adapter customAdapter = new Grid_Adapter(getActivity(), imgList);
+        rv.setAdapter(customAdapter); // set the Adapter to RecyclerView
+        // set a GridLayoutManager with 3 number of columns , horizontal gravity and false value for reverseLayout to show the items from start to end
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity().getApplicationContext(),2, LinearLayoutManager.HORIZONTAL,false);
+        rv.setLayoutManager(gridLayoutManager); // set LayoutManager to RecyclerView
+        return rv;
     }
 }
